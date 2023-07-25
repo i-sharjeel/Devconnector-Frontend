a91b9e4efd39f7b4cc715780788d918fd35fb2fd
sharjeelafzaal123 <muhammad.sharjeel@codefrenetics.com>
2023-07-25 23:51:22 +0500

    Updated package.json with changes

--- src/components/layout/Loading.js
+++ src/components/layout/Loading.js
@@ -1,24 +1,18 @@
 import { Fragment } from "react";
-import Lottie from "react-lottie";
-import loading from '../../img/loading.json'
 
 const Loading = () => {
-    const defaultOptions = {
-        loop: true,
-        autoplay: true,
-        animationData: loading,
-        rendererSettings: {
-            preserveAspectRatio: "xMidYMid slice"
-        }
-    };
+    // const defaultOptions = {
+    //     loop: true,
+    //     autoplay: true,
+    //     animationData: loading,
+    //     rendererSettings: {
+    //         preserveAspectRatio: "xMidYMid slice"
+    //     }
+    // };
     return <>
         <Fragment>
             <div style={{ height: "100vh", display: "flex", alignItems: "center" }}>
-                <Lottie
-                    options={defaultOptions}
-                    height={200}
-                    width={200}
-                />
+                <h3><b>Loading...</b></h3>
             </div>
         </Fragment>
     </>
